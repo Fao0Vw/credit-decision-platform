1442389cff896d44e15dcdba74bb62f2899e4a70
Alexey Fedorov <al.u.fedorov@gmail.com>
2026-05-06 18:44:49 +0400

    added logging

--- services/api-gateway/cmd/main.go
+++ services/api-gateway/cmd/main.go
@@ -9,11 +9,13 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("request: %s %s %s", r.Proto, r.Method, r.URL.Path)
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
 
 	mux.HandleFunc("/score", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("request: %s %s %s", r.Proto, r.Method, r.URL.Path)
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`{"score": 700}`))
 	})
