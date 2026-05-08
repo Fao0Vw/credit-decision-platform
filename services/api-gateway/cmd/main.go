dd35fd2c41f0974ac6742dc89c9f4d615a5af9a2
Alexey Fedorov <al.u.fedorov@gmail.com>
2026-05-08 15:50:16 +0400

    Added livenes/readeness probes; Env vars; JSON encoder

--- services/api-gateway/cmd/main.go
+++ services/api-gateway/cmd/main.go
@@ -1,27 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	mux := http.NewServeMux()
+	port := os.Getenv("PORT")
 
+	if port == "" {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+	hostname, _ := os.Hostname()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("request: %s %s %s", r.Proto, r.Method, r.URL.Path)
+		log.Printf("request: %s %s %s %s", hostname, r.Proto, r.Method, r.URL.Path)
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
 
+	type ScoreResponse struct {
+		Score string `json:"score"`
+	}
+
 	mux.HandleFunc("/score", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("request: %s %s %s", r.Proto, r.Method, r.URL.Path)
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"score": 700}`))
+		score := os.Getenv("DEFAULT_SCORE")
+		if score == "" {
+			score = "770"
+		}
+		response := ScoreResponse{
+			Score: score,
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
 	})
 
-	log.Println("Server started on :8080")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	mux.HandleFunc("/read", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("request: %s %s %s", r.Proto, r.Method, r.URL.Path)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("readOK"))
+	})
+
+	log.Printf("Server started on :%s", port)
+	if err := http.ListenAndServe(":"+port, mux); err != nil {
 		log.Fatal(err)
 	}
 }
